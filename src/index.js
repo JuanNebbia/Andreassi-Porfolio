be5019ac1fda62c41fed14870a44a2ed0d7a86ba
Juan Nebbia <juan.nebbia@gmail.com>
2023-04-30 14:47:14 -0300

    Add firebase enviroment variables for firebase

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8yAX95FPFRlesf0RNV-fjev_rhzxZWe0",
-  authDomain: "andreassi-porfolio.firebaseapp.com",
-  projectId: "andreassi-porfolio",
-  storageBucket: "andreassi-porfolio.appspot.com",
-  messagingSenderId: "632042158252",
-  appId: "1:632042158252:web:85ab0c55c84cf76cacc709",
-  measurementId: "G-NN94NSFMHD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID
 };
 
 
@@ -28,7 +28,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
